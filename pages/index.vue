976b36b0c7fb2cb309a723fb0089ff90b8b1facc
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-07-15 08:32:25 +0900

    Add: #1 pages/index.vueに雛形を追加

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: '',
+
+  setup() {
+    /**
+     * init
+     */
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
