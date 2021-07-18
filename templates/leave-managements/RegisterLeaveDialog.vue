d968d46868e63df9f687eb29051898591406b4b5
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-07-18 13:07:43 +0900

    Add: #5 有給登録ダイアログを作成。(詳細部分については別のissueで対応する)

---
+++ templates/leave-managements/RegisterLeaveDialog.vue
@@ -0,0 +1,27 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {}
+})
+
+export const setRegisterLeaveDialogCtx = () => {
+  const isOpendRegisterLeaveDialog = ref(false)
+
+  const openRegisterLeaveDialog = () => {
+    isOpendRegisterLeaveDialog.value = true
+  }
+
+  const closeRegisterLeaveDialog = () => {
+    isOpendRegisterLeaveDialog.value = false
+  }
+
+  return {
+    isOpendRegisterLeaveDialog,
+    openRegisterLeaveDialog,
+    closeRegisterLeaveDialog
+  }
+}
+</script>
