598853016ae378b381341220a315562bd6d03fbc
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-07-18 13:23:06 +0900

    Add: #5 有給登録のコンポーネントを追加

--- templates/leave-managements/RegisterLeaveDialog.vue
+++ templates/leave-managements/RegisterLeaveDialog.vue
@@ -1,27 +1,9 @@
 <template></template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@nuxtjs/composition-api'
+import { defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup() {}
 })
-
-export const setRegisterLeaveDialogCtx = () => {
-  const isOpendRegisterLeaveDialog = ref(false)
-
-  const openRegisterLeaveDialog = () => {
-    isOpendRegisterLeaveDialog.value = true
-  }
-
-  const closeRegisterLeaveDialog = () => {
-    isOpendRegisterLeaveDialog.value = false
-  }
-
-  return {
-    isOpendRegisterLeaveDialog,
-    openRegisterLeaveDialog,
-    closeRegisterLeaveDialog
-  }
-}
 </script>
