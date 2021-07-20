23f4760578e5362a331d10f7b97ff062c7690c98
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-07-21 08:55:08 +0900

    Feat: #23 ローディング画面のコンポーネントを作成

---
+++ components/AppLoader.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-overlay class="app-loader text-center" :value="isLoading" color="grey lighten-5">
+    <v-progress-circular indeterminate size="100" color="primary" width="12" />
+    <p class="mt-6 primary--text caption font-weight-bold text-center">
+      {{ message }}
+    </p>
+  </v-overlay>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'AppLoader',
+  props: {
+    message: {
+      type: String,
+      required: true
+    },
+    isLoading: {
+      type: Boolean,
+      default: false
+    }
+  }
+})
+</script>
+
+<style scoped></style>
