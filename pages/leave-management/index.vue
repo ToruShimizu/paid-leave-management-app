b8d14b01dc44e53c7fdf713640ca27e84f5a327b
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-07-24 09:12:37 +0900

    Update: #33 カレンダーの表記を日本語に変更

--- pages/leave-management/index.vue
+++ pages/leave-management/index.vue
@@ -16,7 +16,16 @@
       </v-sheet>
     </v-toolbar>
     <v-sheet height="600">
-      <v-calendar ref="calenderRef" v-model="date" color="primary" type="month" @click:date="openRegisterLeaveDialog" />
+      <v-calendar
+        ref="calenderRef"
+        v-model="date"
+        color="primary"
+        type="month"
+        locale="ja-jp"
+        :day-format="timestamp => new Date(timestamp.date).getDate()"
+        :month-format="timestamp => new Date(timestamp.date).getMonth() + 1 + ' /'"
+        @click:date="openRegisterLeaveDialog"
+      />
     </v-sheet>
     <LazyRegisterLeaveDialog v-model="isOpenedRegisterLeaveDialog" :selected-date="date" />
   </div>
